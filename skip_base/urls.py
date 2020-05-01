a477aacb9ef212e25f4a1cded3d2b7f603052bb0
Lindy Lindstrom <llindstrom@lco.global>
2020-05-01 19:40:37 +0000

    most basic DRF implementation: careful POST is active!

--- skip_base/urls.py
+++ skip_base/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from skip import views
+
+router = routers.DefaultRouter()
+router.register(r'targets', views.TargetViewSet)
+router.register(r'events', views.EventViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Wire up our API using automatic URL routing.
+    # Additionally, we include login URLs for the browsable API.
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
