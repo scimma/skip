b13f0a2b83d7287e2b1b8adaf756d8d267c4f5ef
Lindy Lindstrom <llindstrom@lco.global>
2020-05-01 19:42:47 +0000

    add 'api/' to REST endpoints

--- skip_base/urls.py
+++ skip_base/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Wire up our API using automatic URL routing.
     # Additionally, we include login URLs for the browsable API.
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
