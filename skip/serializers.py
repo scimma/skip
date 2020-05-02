9dbdf464abef3c27ad9ecbc239fa87b19b559bfa
Lindy Lindstrom <llindstrom@lco.global>
2020-05-02 01:23:04 +0000

    helpful comments (no functional change - nfc)

--- skip/serializers.py
+++ skip/serializers.py
@@ -8,6 +8,8 @@ class TargetSerializer(serializers.ModelSerializer):
         fields = ['name']
 
 
+# TOOD: use field exclusion instead of inclusion:
+#   see https://www.django-rest-framework.org/api-guide/serializers/#specifying-which-fields-to-include
 # TODO: ?consider serializers.HyperlinkedModelSerializer??
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
--- skip_base/settings.py
+++ skip_base/settings.py
@@ -142,7 +142,10 @@ HOPSKOTCH_CONSUMER_CONFIGURATION = {
     'security.protocol': 'sasl_ssl',
     'sasl.mechanism': 'PLAIN',
     'sasl.username': os.getenv('HOPSKOTCH_SASL_USERNAME', 'test'),
-    'sasl.password': os.getenv('HOPSKOTCH_SASL_PASSWORD', '')    
+    'sasl.password': os.getenv('HOPSKOTCH_SASL_PASSWORD', '')
+    # system dependency: ssl.ca.location may need to be set
+    # this does not seem to be necessary on Ubuntu. However,
+    # for example on centos7: 'ssl.ca.location': '/etc/ssl/certs/ca-bundle.crt',
 }
 
 HOPSKOTCH_TOPICS = ['gcn']
