9884234a160d511d48ef4a9aaef56f1a29f00eff
David Collom <dcollom@lco.global>
2020-05-01 17:28:04 -0700

    config changes reflected in ingestion script

--- skip/management/commands/ingestmessages.py
+++ skip/management/commands/ingestmessages.py
@@ -4,31 +4,18 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from confluent_kafka import Consumer
+from skip.parser import Parser
 
 
-HOPSKOTCH_SERVER = settings.HOPSKOTCH_SERVER
-HOPSKOTCH_PORT = settings.HOPSKOTCH_PORT
-HOPSKOTCH_GROUP = settings.HOPSKOTCH_GROUP
-HOPSKOTCH_AUTO_OFFSET_RESET = settings.HOPSKOTCH_AUTO_OFFSET_RESET
+HOPSKOTCH_CONSUMER_CONFIGURATION = settings.HOPSKOTCH_CONSUMER_CONFIGURATION
 HOPSKOTCH_TOPICS = settings.HOPSKOTCH_TOPICS
-HOPSKOTCH_SECURITY_PROTOCOL = settings.HOPSKOTCH_SECURITY_PROTOCOL
-HOPSKOTCH_SASL_MECHANISM = settings.HOPSKOTCH_SASL_MECHANISM
-HOPSKOTCH_SASL_USERNAME = settings.HOPSKOTCH_SASL_USERNAME
-HOPSKOTCH_SASL_PASSWORD = settings.HOPSKOTCH_SASL_PASSWORD
 
 
 class Command(BaseCommand):
 
     def __init__(self, *args, **kwargs):
-        self.consumer = Consumer({
-            'bootstrap.servers': f'{HOPSKOTCH_SERVER}:{HOPSKOTCH_PORT}',
-            'group.id': HOPSKOTCH_GROUP,
-            'auto.offset.reset': HOPSKOTCH_AUTO_OFFSET_RESET,
-            'security.protocol': HOPSKOTCH_SECURITY_PROTOCOL,
-            'sasl.mechanism': HOPSKOTCH_SASL_MECHANISM,
-            'sasl.username': HOPSKOTCH_SASL_USERNAME,
-            'sasl.password': HOPSKOTCH_SASL_PASSWORD,            
-        })
+        print(HOPSKOTCH_CONSUMER_CONFIGURATION)
+        self.consumer = Consumer(HOPSKOTCH_CONSUMER_CONFIGURATION)
 
     def handle(self, *args, **options):
 
@@ -45,7 +32,9 @@ class Command(BaseCommand):
             
             decoded_message = msg.value().decode('utf-8')
             packet = json.loads(decoded_message)
-            print(packet)
+            if packet['role'] != 'utility' and packet['role'] != 'test':
+                print(packet)
+
 
 
         self.consumer.close()
