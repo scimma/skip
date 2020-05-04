49f6b970c27a7b69be7f0a0f429124bbc0ffb960
Lindy Lindstrom <llindstrom@lco.global>
2020-05-04 17:54:32 +0000

    update serializers with new model fields

--- skip/serializers.py
+++ skip/serializers.py
@@ -5,11 +5,20 @@ from rest_framework import serializers
 class TargetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Target
-        fields = ['name']
+        fields = ['name', 'right_ascension', 'declination', 'created', 'modified']
 
 
 # TODO: ?consider serializers.HyperlinkedModelSerializer??
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ['target_id', 'message']
+        fields = [  # 'target_id',  
+                  'event_identifier',
+                  'event_timestamp',
+                  'right_ascension',
+                  'declination',
+                  'role',
+                  'topic',
+                  'message',
+                  'created',
+                  'modified']
