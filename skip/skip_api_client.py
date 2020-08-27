cfa60f8c4ae04fc76d12de2913c82914b62fb5c5
David Collom <dcollom@lco.global>
2020-08-27 10:30:30 -0700

    Added staticfiles_finders

--- skip/skip_api_client.py
+++ skip/skip_api_client.py
@@ -17,11 +17,12 @@ SKIP_API_KEY = settings.SKIP_API_KEY
 class SkipORMClient(SkipAPIClient):
 
     def get_alerts(self, *args, **kwargs):
-        page = kwargs.pop('page', 1)
+        page_num = kwargs.pop('page', 1)
         page_size = kwargs.pop('page_size', 20)
         af = AlertFilter(kwargs, queryset=Alert.objects.all())
         paginator = Paginator(af.qs, page_size)
-        alerts = AlertSerializer(paginator.page(page).object_list, many=True)
+        alerts = AlertSerializer(paginator.page(page_num).object_list, many=True)
 
         # TODO: find a way to return as dict for performance rather than needing to do json.loads()
+        # TODO: or figure out why OrderedDict doesn't work with Dash
         return json.loads(JSONRenderer().render(alerts.data))
--- skip_base/settings.py
+++ skip_base/settings.py
@@ -154,6 +154,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+
+    'django_plotly_dash.finders.DashAssetFinder',
+    'django_plotly_dash.finders.DashComponentFinder',
+    'django_plotly_dash.finders.DashAppDirectoryFinder',
+]
 
 # Django REST Framework configuration
 
