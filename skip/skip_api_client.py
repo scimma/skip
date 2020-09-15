d9554dd3cdcd063ac7dbb08919e0194c76c9e968
David Collom <dcollom@lco.global>
2020-09-15 11:49:26 -0700

    Added default ordering and nulls_last=True

--- skip/skip_api_client.py
+++ skip/skip_api_client.py
@@ -20,7 +20,7 @@ class SkipORMClient(SkipAPIClient):
     def get_alerts(self, *args, **kwargs):
         page_num = kwargs.pop('page', 1)
         page_size = kwargs.pop('page_size', 20)
-        af = AlertFilter(kwargs, queryset=Alert.objects.all())
+        af = AlertFilter(kwargs, queryset=Alert.objects.all().order_by(F('alert_timestamp').desc(nulls_last=True)))
         paginator = Paginator(af.qs, page_size)
         alerts = AlertSerializer(paginator.page(page_num).object_list, many=True)
 
